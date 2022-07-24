86e2963489a4089169eea84593f205895186692e
Fabio <fabio@screendragon.com>
2022-07-24 22:39:38 +0200

    add stylus file for printing

--- server.js
+++ server.js
@@ -44,7 +44,7 @@ const port = process.env.PORT || 5000
 
 const server = app.listen(port, () => {
 
-    console.log(`App running on port: ${port}`)
+    console.log(`App running on port: http://localhost:${port}`)
 
 })
 
