c623155ccd81159fdf05e0f21cedffb9f8cac151
Fabio <fabio@screendragon.com>
2023-03-12 12:57:51 +0100

    add cors for deploy

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import morgan from 'morgan'
 import dotenv from "dotenv"
+import cors from "cors"
 import path from 'path'
 import { fileURLToPath } from 'url'
 import { dirname } from 'path'
@@ -21,6 +22,17 @@ if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'));
 
 }
+
+app.use(cors({
+    origin: 'https://pfcode-wifi-qr.onrender.com/'
+}));
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 
 app.use(express.json())
 
